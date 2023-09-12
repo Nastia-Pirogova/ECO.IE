e367803658eac0856856471f174077ca5e20d591
Anastasia Pirogova <109249070+Nastia-Pirogova@users.noreply.github.com>
2023-09-12 22:57:39 +0300

    1

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,27 @@ let dropDowns = document.querySelectorAll(".faq-inner__list-item-descr");
 let minusIcon = document.querySelectorAll(".faq-inner__list-item-svg-minus");
 let plusIcon = document.querySelectorAll(".faq-inner__list-item-svg-plus");
 
-
-// Add a click event listener to each title element
 titlePrograms.forEach((titleProgram, index) => {
     titleProgram.addEventListener("click", () => {
-        // Toggle the "spec-programs-item__content-active" class on the corresponding dropdown element
         dropDowns[index].classList.toggle("faq-inner__list-item-descr-active");
         titlePrograms[index].classList.toggle("faq-inner__list-question-active");
         minusIcon[index].classList.toggle("svg-minus-active");
         plusIcon[index].classList.toggle("svg-plus-active");
     });
 });
+
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
